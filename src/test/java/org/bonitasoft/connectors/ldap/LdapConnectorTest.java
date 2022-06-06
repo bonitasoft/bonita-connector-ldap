422efc602c76a618144100fb97073f57b7bdfa7a
Romain Bioteau <romain.bioteau@bonitasoft.com>
2022-06-06 17:17:38 +0200

    tr(deps): add dependabot updates

--- src/test/java/org/bonitasoft/connectors/ldap/LdapConnectorTest.java
+++ src/test/java/org/bonitasoft/connectors/ldap/LdapConnectorTest.java
@@ -18,9 +18,9 @@
 package org.bonitasoft.connectors.ldap;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.assertj.core.api.Assertions;
